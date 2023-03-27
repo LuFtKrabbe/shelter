7225a8ebb58a465b2b6feefb31e61b96b8df4eca
Alexander Filimonov <filimonov.alexander.s@yandex.ru>
2023-03-27 16:40:57 +0300

    feat: add links and implement animation

--- shelter/src/js/index.js
+++ shelter/src/js/index.js
@@ -2,8 +2,8 @@ console.log (
 	`
 	Self-estimation of work:\n
 	Score: 0 / 100\n
-	- [x] Something is done;
-	- [x] Something is done;
-	- [x] Something is done.
+	- [x] Something is done;\n
+	- [x] Something is done;\n
+	- [x] Something is done.\n
 	`
 );
