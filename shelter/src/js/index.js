75ab52b7e2a8ae44e08c53b0ec5dc157fef85be9
Alexander Filimonov <filimonov.alexander.s@yandex.ru>
2023-03-27 21:21:39 +0300

    fix: correct animation and delete preload of the fonts

--- shelter/src/js/index.js
+++ shelter/src/js/index.js
@@ -2,7 +2,7 @@ console.log (
 	`
 	Self-estimation of work:\n
 	Score: 100 / 100\n
-	- [x] Valid Layout: document's checking is completed. No errors or warnings to show. (14/14);
+	- [x] Valid Layout: document's checking is completed. No errors or warnings to show (14/14);
 	- [x] Layout is completely according to the sketch (50/50);
 	- [x] CSS requirements: implemented with CSS Flex, icons and images are displayed correctly (10/10);
 	- [x] Smooth scrolling and animation for elements are added (26/26).
