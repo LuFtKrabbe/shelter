e2f08b8302357e0cd0da4f962ca2a41978d77e68
Alexander Filimonov <filimonov.alexander.s@yandex.ru>
2023-04-03 15:43:47 +0300

    fix: refresh self-estimation according to the task

--- shelter/src/js/index.js
+++ shelter/src/js/index.js
@@ -2,9 +2,11 @@ console.log (
 	`
 	Self-estimation of work:\n
 	Score: 100 / 100\n
-	- [x] Valid Layout: document's checking is completed. No errors or warnings to show (14/14);
-	- [x] Layout is completely according to the sketch (50/50);
-	- [x] CSS requirements: implemented with CSS Flex, icons and images are displayed correctly (10/10);
-	- [x] Smooth scrolling and animation for elements are added (26/26).
+	- [x] The Main's page layout is completely according to the sketch (42/42);
+	- [x] The Pets' page layout is completely according to the sketch (18/18);
+	- [x] There is no deformation of the sketch and horizontal scroll-line (20/20);
+	- [x] The design of the pages is responsive (8/8);
+	- [x] The hambueger menu button is implemented (4/4);
+	- [x] Valid Layout: document's checking is completed (8/8).
 	`
 );
